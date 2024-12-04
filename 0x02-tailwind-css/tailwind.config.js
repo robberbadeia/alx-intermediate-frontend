9f13ecf1e7264240d6546d2a39e07bd721500b73
robberbadeia <robber.badeia@gmail.com>
2024-12-04 05:21:02 -0800

    Flexbox Basics - Build a Simple Navigation Bar

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
     content: ["./src/**/*.{html,js}",
       "./1-index.html",
-      "./2-index.html"
+      "./2-index.html",
+      "./3-nav_index.html"
     ],
-    
+
     theme: {
       extend: {},
     },
