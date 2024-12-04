78faa1e63698bc30fa1a16d029b04c4b94290d92
robberbadeia <robber.badeia@gmail.com>
2024-12-04 06:10:01 -0800

    Build a Responsive Image Gallery Using CSS Grid

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
       "./2-index.html",
       "./3-nav_index.html",
       "./4-flexbox_index.html",
-      "./5-gridflex_index.html"
+      "./5-gridflex_index.html",
+      "./6-imageGallery.html"
     ],
 
     theme: {
